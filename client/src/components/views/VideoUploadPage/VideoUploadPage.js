c32c72a7acc0681e468fc2c4611818eb3994cc3c
leyuri <leyuri97@gmail.com>
2020-02-27 03:32:42 +0900

    Generate Video Thumbnail

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,10 @@
 import React , {useState} from 'react'
 import { Typography, Button, Form, message, Input, Icon} from'antd';
-import TextArea from 'antd/lib/input/TextArea';
 import Dropzone from 'react-dropzone';
 import axios from 'axios';
-import { use } from 'bcrypt/promises';
 
-// const { TextArea } = Input;
+
+const { TextArea } = Input;
 const { Title } = Typography;
 
 
@@ -18,7 +17,8 @@ const CategoryOption = [
     {value: 0, label: "Film & Animation"},
     {value: 1, label: "Autos & Vehicles"},
     {value: 2, label: "Music"},
-    {value: 3, label: "Pets & Animals"}
+    {value: 3, label: "Pets & Animals"},
+    {value: 4, label: "Sports" },
 ]
 
 function VideoUploadPage() {
@@ -29,6 +29,9 @@ function VideoUploadPage() {
     const [Description, setDescription] = useState("")
     const [Private, setPrivate] = useState(0)
     const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [Thumbnail, setThumbnail] = useState("")
 
     const onTitleChange = (e) => {
         setVideoTitle(e.currentTarget.value)
@@ -46,54 +49,79 @@ function VideoUploadPage() {
         setCategory(e.currentTarget.value)
     }
 
-    const onDrop = (files) => {
+    const onSubmit = () => {
+        
+    }
+//Axios ajax와 비슷, 같이 보내주지 않으면 파일을 보낼 때 오류가 생김, 따라서 헤더에다가 content-type을 지정해줘야 한다
 
-        let formData = new FormData;
+    const onDrop = ( files ) => {
+
+        let formData = new FormData();
         const config = {
-            header: {'content-type': 'multipart/form-data'}
+            header: { 'content-type': 'multipart/form-data' }
         }
-        formData.append("file",files[0])
+        console.log(files)
+        formData.append("file", files[0])
 
-        //Axios ajax와 비슷, 같이 보내주지 않으면 파일을 보낼 때 오류가 생김, 따라서 헤더에다가 content-type을 지정해줘야 한다
         axios.post('/api/video/uploadfiles', formData, config)
-            .then(response => {
-                if(response.data.success) {
+        .then(response=> {
+            if(response.data.success){
 
-                }else{
-                    alert('Video upload failed.')
+                let variable = {
+                    filePath: response.data.filePath,
+                    fileName: response.data.fileName
                 }
-            })
+                setFilePath(response.data.filePath)
+
+                //gerenate thumbnail with this filepath ! 
+                
+                axios.post('/api/video/thumbnail', variable)
+                .then(response => {
+                    if(response.data.success) {
+                        setDuration(response.data.fileDuration)
+                        setThumbnail(response.data.thumbsFilePath)
+                    } else {
+                        alert('Failed to make the thumbnails');
+                    }
+                })
+
+
+            } else {
+                alert('failed to save the video in server')
+            }
+        })
+
     }
 
     return (
-        <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
-            <div style={{ textAlign: 'center', marginBottom:'2rem' }}>
-                <Title level={2}>Upload Video</Title>
-            </div>
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+        <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+            <Title level={2} > Upload Video</Title>
+        </div>
 
-            <Form onSubmit>
-                <div style={{ display:'flex', justifyContent:'space-between' }}></div>
-                {/* Drop Zone */}
+        <Form onSubmit={onSubmit}>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Dropzone 
+                    onDrop={onDrop}
+                    multiple={false}
+                    maxSize={800000000}>
+                    {({ getRootProps, getInputProps }) => (
+                        <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                            {...getRootProps()}
+                        >
+                            <input {...getInputProps()} />
+                            <Icon type="plus" style={{ fontSize: '3rem' }} />
 
-                <Dropzone
-                onDrop={onDrop}
-                multiple={false}    //올릴 동영상 개수(1개씩)
-                maxSize={10000000} //사이즈 조정
-                >
-                {({ getRootProps, getInputProps}) => (
-                    <div style={{ width: '700px', height: '240px', border:'1px solid lightgray', display:'flex',
-                    alignItems:'center', justifyContent:'center'}} {...getRootProps()}>
-                        <input {...getInputProps()}/>
-                        <Icon type="plus" style={{ fontSize:'4rem'}}/> 
-                    </div>
-                )}
+                        </div>
+                    )}
                 </Dropzone>
 
-                {/* Thumbnail */}
-
-                <div>
-                    <img src alt/>
-                </div>
+                {Thumbnail !== "" &&
+                    <div>
+                        <img src={`http://localhost:5000/${Thumbnail}`} alt="haha" />
+                    </div>
+                }
+            </div>
 
             <br />
             <br />
--- server/routes/video.js
+++ server/routes/video.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const { User } = require("../models/User");
 const { auth } = require("../middleware/auth");
 const multer = require("multer");
+var ffmpeg = require("fluent-ffmpeg");
 
 
 
@@ -38,12 +39,48 @@ router.post("/uploadfiles", (req, res) => {
         if (err) {
             return res.json({ success: false, err }) //실패시
         }
-        return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename }) //성공시
-        
+        return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
     })
-
 });
 
+router.post("/thumbnail", (req, res) => {
+    // 클라이언트에서 받은 썸네일 생성 & 비디오 런닝타임 서버에 저장한다.
+
+    let thumbsFilePath ="";
+    let fileDuration ="";
+
+
+    //비디오 정보를 가져오기
+    ffmpeg.ffprobe(req.body.filePath, function(err, metadata){
+        console.dir(metadata);
+        console.log(metadata.format.duration);
+
+        fileDuration = metadata.format.duration;
+    })
+
+    //썸네일 생성
+    ffmpeg(req.body.filePath)
+        .on('filenames', function (filenames) {
+            console.log('Will generate ' + filenames.join(', '))
+            thumbsFilePath = "uploads/thumbnails/" + filenames[0];
+        })
+        .on('end', function () {
+            console.log('Screenshots taken');
+            return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration})
+        })
+        .screenshots({
+            // Will take screens at 20%, 40%, 60% and 80% of the video
+            count: 3, //3개의 썸네일이 생긴다
+            folder: 'uploads/thumbnails',
+            size:'320x240',
+            // %b input basename ( filename w/o extension )
+            filename:'thumbnail-%b.png'
+        });
+
+    
+});
+    
+    
 
 
 module.exports = router;
