1edd6432c5d70a6f5d1dd47fb062b6378fedba97
leyuri <leyuri97@gmail.com>
2020-02-28 03:20:37 +0900

    Add Subscribe Button UI

---
+++ client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Subscribe() {
+    return (
+        <div>
+            <button
+                style={{
+                    backgroundColor: '#CC0000',
+                    borderRadius: '4px',
+                    color: 'white', padding: '10px 16px',
+                    fontWeight: '500', fontSize: '1rem', textTransform: 'uppercase'
+                }}
+                onClick
+                >
+                   0 Suberscribe    
+            </button>
+        </div>
+    )
+}
+
+export default Subscribe;
