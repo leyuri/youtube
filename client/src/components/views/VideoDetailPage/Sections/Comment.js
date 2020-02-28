f7738977e4c94e08b254a1865054804b2fb41609
leyuri <leyuri97@gmail.com>
2020-02-29 02:30:34 +0900

    Change Comment Template

--- client/src/components/views/VideoDetailPage/Sections/Comment.js
+++ client/src/components/views/VideoDetailPage/Sections/Comment.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Button, Input } from 'antd';
+
+const { TextArea } = Input;
+
 
 const Comment = () => {
+
+
     return (
         <div>
-            댓글
+            <br />
+            <p> replies</p>
+            <hr />
+            {/* Comment Lists  */}
+   
+            {/* Root Comment Form */}
+            <form style={{ display: 'flex' }} onSubmit >
+                <TextArea
+                    style={{ width: '100%', borderRadius: '5px' }}
+                    onChange
+                    value
+                    placeholder="write some comments"
+                />
+                <br />
+                <Button style={{ width: '20%', height: '52px' }} onClick >Submit</Button>
+            </form>
+
         </div>
     )
 }
