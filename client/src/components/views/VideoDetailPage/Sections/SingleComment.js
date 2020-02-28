cbc102fd975e87ed4f0c527df060a5abc764f69d
leyuri <leyuri97@gmail.com>
2020-02-29 03:16:36 +0900

    Add SingleComment Template

--- client/src/components/views/VideoDetailPage/Sections/SingleComment.js
+++ client/src/components/views/VideoDetailPage/Sections/SingleComment.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Comment, Avatar, Button, Input } from 'antd';
+
+const { TextArea } = Input;
 
 function SingleComment() {
     return (
         <div>
-            Singggg
+            <Comment
+                actions
+                author
+                avatar={<Avatar src alt/>}
+                content
+            >
+
+            </Comment>
+
+   
+                <form style={{ display: 'flex' }} onSubmit>
+                    <TextArea
+                        style={{ width: '100%', borderRadius: '5px' }}
+                        onChange
+                        value
+                        placeholder="write some comments"
+                    />
+                    <br />
+                    <Button style={{ width: '20%', height: '52px' }} onClick>Submit</Button>
+                </form>
+  
+
         </div>
     )
 }
