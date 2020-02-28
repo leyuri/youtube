5603ac5ff931c26855c8026f2fe591f85435f012
leyuri <leyuri97@gmail.com>
2020-02-28 22:51:47 +0900

    Get SubscriperInfo

--- server/routes/video.js
+++ server/routes/video.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 var ffmpeg = require('fluent-ffmpeg');
 
 const { Video } = require("../models/Video");
+const { Subscriber} = require("../models/Subscriber")
 
 const { auth } = require("../middleware/auth");
 
@@ -115,6 +116,43 @@ router.post("/getVideo", (req, res) => {
         res.status(200).json({ success: true, video })
     })
 });
+
+
+router.post("/getSubscriptionVideos", (req, res) => {
+//자신의 아이디를 가지고 구독하는 사람들을 찾는다.
+
+    Subscriber.find({ userFrom: req.body.userFrom})
+    .exec(( err, subscribeInfo) => {
+
+        if(err) return res.status(400).send(err);
+
+        let subscribedUser = [];
+
+        subscribeInfo.map((subscriber, i)=> {
+            subscribedUser.push(subscriber.userTo);
+        })
+
+        //찾은 사람들의 비디오를 가지고 온다. 
+        //$in 몽고디비가 갖고 있는 !
+
+        Video.find({writer : { $in: subscribedUser}})
+        .populate('writer')
+        .exec((err, videos) => {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        })
+
+    })
+
+
+
+
+
+});
+
+
+
+
 
 
 module.exports = router;
