0941cae72f6b2de7fac8bc4ea7466982c603d5f2
leyuri <leyuri97@gmail.com>
2020-03-02 21:04:49 +0900

    Add Like collections

---
+++ server/models/Like.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const LikeSchema = mongoose.Schema({
+
+
+    userId : {
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    },
+    commentId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    },
+    videoId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Video'
+    }
+
+
+}, { timestamps: true })
+
+
+const Like = mongoose.model('Like', LikeSchema);
+
+module.exports = { Like }
