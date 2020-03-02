a6d046cdd5cafd8b673b9fed2fdfc82e1ea12032
leyuri <leyuri97@gmail.com>
2020-03-02 22:19:05 +0900

    Add Like & Dislike Button

--- client/src/components/views/VideoDetailPage/Sections/LikeDislikes.js
+++ client/src/components/views/VideoDetailPage/Sections/LikeDislikes.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Tooltip, Icon } from 'antd';
 
 function LikeDislikes() {
     return (
-        <div>
-            LikeDislikes
-        </div>
+        <React.Fragment>
+            <span key="comment-basic-like">
+                <Tooltip title="Like">
+                    <Icon type="like"
+                        theme="filled"
+                        onClick />
+                </Tooltip>
+                <span style={{ paddingLeft: '8px', cursor: 'auto' }}></span>
+            </span>&nbsp;&nbsp;
+            <span key="comment-basic-dislike">
+                <Tooltip title="Dislike">
+                    <Icon
+                        type="dislike"
+                        theme="outlined"
+                        onClick
+                    />
+                </Tooltip>
+                <span style={{ paddingLeft: '8px', cursor: 'auto' }}></span>
+            </span>
+        </React.Fragment>
     )
 }
 
