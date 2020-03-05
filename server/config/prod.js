839d27856dc289b3af01bdb8c8e45ca55ab58857
leyuri <leyuri97@gmail.com>
2020-03-06 03:19:30 +0900

    Change prod

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,4 @@
-// var MONGO_URI="mongodb://dbuser:dbUser1123@ds039467.mlab.com:39467/youtube";
 module.exports = {
-    mongoURI:process.env.MONGO_URI="mongodb://dbuser:dbUser1123@ds039467.mlab.com:39467/youtube"
+    mongoURI:process.env.MONGO_URI
   
 }
